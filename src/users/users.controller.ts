28bed2f4b4f95a5ab6415830f906571a063dcb1c
Gosse <thms.gosse@gmail.com>
2019-03-20 21:15:08 +0100

    feat(auth): create http strategy to secure app and use a guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Delete, Body, Param, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param, Res, HttpStatus, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Artwork } from './types/artwork.type';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get(':userId/:artworkType')
+  @UseGuards(AuthGuard('bearer'))
   getArtworksByType(
     @Param('userId') userId,
     @Param('artworkType') type,
