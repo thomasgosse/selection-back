a6e334d6cf2ea8e2b921eecef5c959a35167ade5
Gosse <thms.gosse@gmail.com>
2019-03-28 13:17:38 +0100

    feat(auth): handle spotify token retrieving

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,36 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, HttpService } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import * as querystring from 'querystring';
+
+const authConfig = require('./authConfig.json');
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly httpService: HttpService) {}
 
   async validateUser(token: string): Promise<any> {
-    return admin.auth().verifyIdToken(token).catch(() => { throw new UnauthorizedException(); });
+    return admin.auth().verifyIdToken(token)
+      .then(decodedToken => (decodedToken.aud === authConfig.selectionId) ? true : false)
+      .catch(() => { throw new UnauthorizedException(); });
+  }
+
+  getSpotifyToken(): Promise<any> {
+    const requestBody = querystring.stringify({ grant_type: 'client_credentials' });
+    const config = {
+      headers: {
+        Authorization: 'Basic '.concat(authConfig.selectionBasic),
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+    return new Promise((resolve, reject) => {
+      this.httpService.post('https://accounts.spotify.com/api/token', requestBody, config)
+       .subscribe(
+          (result) => {
+            resolve(result.data.access_token);
+          },
+          (error) => {
+            reject(error);
+          });
+    });
   }
 }
